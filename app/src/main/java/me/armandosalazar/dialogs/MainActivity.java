88555dc4371ab3f5e45db0aa6f117c59e4c212e6
Armando Salazar <armandosalazar351@gmail.com>
2023-06-05 09:11:48 -0600

    date dialog works

--- app/src/main/java/me/armandosalazar/dialogs/MainActivity.java
+++ app/src/main/java/me/armandosalazar/dialogs/MainActivity.java
@@ -3,6 +3,7 @@ package me.armandosalazar.dialogs;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import com.google.android.material.textfield.TextInputLayout;
@@ -27,13 +28,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void inflateViews() {
-        // calendar = Calendar.getInstance(TimeZone.getTimeZone("CDT"));
-        calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        calendar = Calendar.getInstance(TimeZone.getTimeZone("CDT"));
+        // calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
         textInputLayoutResponse = findViewById(R.id.textInputLayoutResponse);
     }
 
     private void setEvents() {
         findViewById(R.id.btnAlert).setOnClickListener(v -> showAlertDialog());
+        findViewById(R.id.btnDate).setOnClickListener(v -> showDatePickerDialog());
     }
 
 
@@ -50,5 +52,15 @@ public class MainActivity extends AppCompatActivity {
             dialog.dismiss();
         });
         builder.show();
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, (view, year, month, dayOfMonth) -> {
+            this.day = calendar.get(Calendar.DAY_OF_MONTH);
+            this.month = calendar.get(Calendar.MONTH);
+            this.year = calendar.get(Calendar.YEAR);
+            textInputLayoutResponse.getEditText().setText(day + "/" + month + "/" + year);
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
     }
 }
